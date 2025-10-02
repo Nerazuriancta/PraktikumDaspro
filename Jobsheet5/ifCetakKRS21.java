524354ef1dad6615ec27aa36358b40d4f6909e89
Nerazuriancta <nerasyahputri@gmail.com>
2025-10-02 17:38:23 +0700

    Modifikasi Percobaan 1

--- Jobsheet5/ifCetakKRS21.java
+++ Jobsheet5/ifCetakKRS21.java
@@ -11,6 +11,8 @@ public class ifCetakKRS21 {
         if (uktLunas) {
             System.out.println("Pembayaran UKT terverifikasi");
             System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
-        } 
+        } else {
+            System.out.println("Registrasi ditolak. Silahkan lunasi UKT terlebih dauhulu. ");
+        }
     }
 }
