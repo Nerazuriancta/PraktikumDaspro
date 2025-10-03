b229de508e963144cd936bc63bbcd1b400ec93df
Nerazuriancta <nerasyahputri@gmail.com>
2025-10-03 12:37:32 +0700

    Tugas 1

--- Jobsheet5/ifCetakKRS21.java
+++ Jobsheet5/ifCetakKRS21.java
@@ -4,7 +4,7 @@ public class ifCetakKRS21 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("--- Cetak KRS SIAKAD ---");
+        System.out.println("--- Cetak KRS SIAKAD ---"); 
         System.out.print("Apakah UKT sudah lunas? (true/false): ");
         boolean uktLunas = sc.nextBoolean();
 
@@ -14,5 +14,10 @@ public class ifCetakKRS21 {
         } else {
             System.out.println("Registrasi ditolak. Silahkan lunasi UKT terlebih dauhulu. ");
         }
+
+        String pesan = (uktLunas) ? "Pembayaran UKT terverifikasi\nSilahkan cetak KRS dan minta tanda tangan DPA" : "Registrasi ditolak. Silahkan lunasi UKT terlebih dauhulu.";
+
+        System.out.println("--- Versi Ternary Operator ---");
+        System.out.println(pesan);
     }
 }
