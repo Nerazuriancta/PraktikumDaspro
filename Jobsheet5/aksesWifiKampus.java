337491b4c22fb891e40d20287a4e67485b8e7599
Nerazuriancta <nerasyahputri@gmail.com>
2025-10-05 18:31:12 +0700

    Tugas 3 no. 2

---
+++ Jobsheet5/aksesWifiKampus.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class aksesWifiKampus {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan jenis pengguna (Mahasiswa/Dosen): ");
+        String jenisPengguna = sc.nextLine().trim();
+
+        if (jenisPengguna.equalsIgnoreCase("dosen")) {
+            System.out.println("Akses WIFI diberikan (dosen)");
+        } else {
+            if (jenisPengguna.equalsIgnoreCase("mahasiswa")) {
+                System.out.print("Masukkan jumlah SKS: ");
+                int jumlahSKS = sc.nextInt();
+
+                if (jumlahSKS >= 12) {
+                    System.out.println("Akses WIFI idberikan (mahasiswa aktif)");
+                } else {
+                    System.out.println("Akses ditolak, SKS kurang dari 12");
+                }
+            } else {
+                System.out.println("Akses ditolak");
+            }
+        }
+    }
+}
